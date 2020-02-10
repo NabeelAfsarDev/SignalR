a3d95d9e66dc84aa1100c927ce97b61be35bf015
Nabeel Afsar <50879095+nadeveloper786@users.noreply.github.com>
2020-02-09 18:05:48 -0600

    Add project files.

---
+++ signalRmvc5Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(signalRmvc5Demo.Startup))]
+namespace signalRmvc5Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
